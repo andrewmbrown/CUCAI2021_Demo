059626ce9d994ffc84f6f65f357c9fe4f515542a
Smeet Chheda <contact.smeet@gmail.com>
2021-02-28 16:37:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,36 @@ if navigation == 'Introduction':
 
 if navigation == 'Options Description':
     st.write("""
-        ## Options Description
+        ## What are options?
+
+        Stock market volatility has been a heavily 
+        discussed topic ever since the inception of the market in the early 1600s. Most people assume that the only time
+        anyone ever makes money is when stocks go up; however, this is far from the case. A profit can be made even when a stock gets
+        bad news or sees itself in the midst of a \'market crash\'. A majority stocks have
+        options contracts whose price is derived from the price of the stock. Options contracts present investors with the ability to 
+        benefit from adverse movement's in an underlying securities\' price.            
     """)
+    st.write("""
+        ## Types of Options Contracts
+        Regardless of the type of contract, all options contracts will have the two following things:
+        - Strike Price
+        - Expiry Date
+        - Premium
+        The strike price is the price at which the contract agreement is executed, whereas the expiry date
+        acts the same way it does on a coupon. A buyer can, but is not obligated to, execute the contract at any point
+        prior to expiry; however, once the contract has reached expiry, it is worthless. The premium can be thought of as the price
+        of the contract. As the underlying stock price moves favorably with respect to the contract terms, the premium of the
+        contract increases. 
+        ### Calls
+        When a trader 
+        
+    
+    """)
+    
+    st.write("""
+    #
+    """)
+
 
 if navigation == 'ARIMA Description':
     st.write("""
